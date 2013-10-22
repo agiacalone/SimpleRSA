3b8ad844c87662533ca8065b5a6a1a00066dead9
Anthony Giacalone <agiacalone@gmail.com>
2013-10-22 15:11:59 -0700

    minor changes to satisfy PyLinter

--- decrypt.py
+++ decrypt.py
@@ -3,12 +3,13 @@
 # decrypt.py
 # A simple RSA decryption program
 
-import sys
-import math
-import cmath
+#import sys
+#import math
+#import cmath
 from lettervalue import *
 
-scanned_file = open('code.txt').read().split()   # read in file and strip lead/trail whitespace
+scanned_file = open('code.txt').read().split()
+    # read in file and strip lead/trail whitespace
 
 e = 48925           # public key
 n = 88579           # public key
@@ -22,29 +23,29 @@ counter = 0
 while counter < len(scanned_file):
     # The code segment to decrypt
     code = int(scanned_file[counter])
-    
+
     # Print out for the user
     #print("The ciphered number is:", code)
-    
+
     # Decipher the encrypted text
-    decipher = pow(code,d) % (p*q)
+    decipher = pow(code, d) % (p*q)
     #print("The unciphered scramble is:", decipher)
-    
+
     # Unscramble the cipher
-    mod_a = int (decipher*pow((1/26),2) % 26)
-    mod_b = int (decipher/26 % 26)
-    mod_c = int (decipher % 26)
+    mod_a = int(decipher*pow((1/26), 2) % 26)
+    mod_b = int(decipher/26 % 26)
+    mod_c = int(decipher % 26)
     #print("The letter values:", mod_a, mod_b, mod_c)
-    
+
     # Get the letters based on the table
     lett_a = lettervalue.getLett(mod_a)
     lett_b = lettervalue.getLett(mod_b)
     lett_c = lettervalue.getLett(mod_c)
     #print("The unscrambled cipher is:", lett_a, lett_b, lett_c)
-    
+
     # Put the letters into an array for further processing
     final_array.append(lett_a + lett_b + lett_c)
-    
+
     # And finally, increase the counter to start the loop over again
     counter += 1
 
--- encrypt.py
+++ encrypt.py
@@ -3,12 +3,13 @@
 # encrypt.py
 # A simple RSA encryption program
 
-import sys
-import math
-import cmath
+#import sys
+#import math
+#import cmath
 from lettervalue import *
 
-scanned_file = open('plaintext.txt').read().split()   # read in file and strip lead/trail whitespace
+scanned_file = open('plaintext.txt').read().split()
+    # read in file and strip lead/trail whitespace
 
 e = 48925           # public key
 n = 88579           # public key
@@ -23,26 +24,26 @@ counter = 0
 while counter < len(scanned_file):
     # The code segment to decrypt
     phrase = scanned_file[counter]
-    
+
     # Print out info for the user
     #print("The letters to encrypt:", phrase[0], phrase[1], phrase[2])
-    
+
     # Get the numeric values for the letters
     val_a = lettervalue.getVal(phrase[0])
     val_b = lettervalue.getVal(phrase[1])
     val_c = lettervalue.getVal(phrase[2])
-    
+
     # Run the scrambler
-    zn = (val_a*pow(26,2)) + val_b*26 + val_c
+    zn = (val_a*pow(26, 2)) + val_b*26 + val_c
     #print("The scrambled code is:", zn)
-    
+
     # Now, RSA the scrambled text
-    cipher = pow(zn,e) % (p*q)
+    cipher = pow(zn, e) % (p*q)
     #print("The RSA cipher is:", cipher)
-    
+
     # Put the cipher into an array for further processing
     final_code.append(cipher)
-       
+
     # And finally, increase the counter to start the loop over again
     counter += 1
 
